5828d56884095cc5fddaa654b0373fd8a8e3140b
BawdenS <mathelatebo@gmail.com>
2023-04-28 18:30:17 -0300

    update planta
    Update da planta agora realizável

--- Oniram/TwoTanks_NL_main.m
+++ Oniram/TwoTanks_NL_main.m
@@ -8,29 +8,40 @@
 
 % Parâmetros do Sistema Não-Linear   
   pho = 998.23; % kg/(m^3)
-  Qe = 2.2183/3; % kg/s
-  Fnom = 25; % Hz
+  Fnom = 30; % Hz
   g = 9.81; % m/(s^3)
-  Area1 = 3.1416; % m^2
-  Area2 = 0.0113; % m^2
-  Area4 = 0.0314; % m^2
-  Rmax = 1; % m
-  Hmax = 3; % m
-    
+  D1 = (5+3/8)*2.54/100; % diametro
+  Area1 = pi*D1^2/4; % m^2
+  H1max = 30*2.54/100;
+  
+  Dout = 0.05;
+  Aout1 = pi*(Dout^2)/4; %0.0113; % m^2
+
+
+  
+% Eq de Bernoulli
+  Qe = pho*Aout1*sqrt(2*g*H1max); % kg/s
+  
+  
+  % Tanque 2
+  Rmax = 0.176; % m
+  Hmax = 0.7; % m
+  Aout2 = Aout1*2/3; % m^2
+  
 % Condições Iniciais do Sistema
-  h10 = 1; h20 = 1;
+  h10 = 0.01; h20 = 0.01;
       
 % Tempo de Simulação
   t0 = 0; dt = 0.01; tfinal = 2000;
   t = t0:dt:tfinal; st = size(t,2);
   
 % Controle em Malha Aberta (F)
-  F_rp = 127.5377 + 0*t;
+  F_rp = 30;
   
-  H1_rp = (F_rp.^6*Qe^2)/(2*Area2^2*Fnom^6*g*pho^2);
-  
-  H2_rp = (Area2^2*H1_rp)/Area4^2;
+%   H1_rp = (F_rp.^6*Qe^2)/(2*Aout^2*Fnom^6*g*pho^2);
+%   
+%   H2_rp = (Aout^2*H1_rp)/Area4^2;
   
 % Executando a simulação
-  open('TwoTanks_NL_2018a'); sim('TwoTanks_NL_2018a');
-  
+  open('TwoTanks_NL_2018a'); 
+  sim('TwoTanks_NL_2018a');
